571b08c5ededdc7ad3a7e531c139c90e303e372c
ViktorM888 <viktormarjanac@gmail.com>
2019-12-22 17:46:13 +0100

    Initial commit

---
+++ SeleniumFirst/PageObject.cs
@@ -0,0 +1,74 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using SeleniumExtras.PageObjects;
+
+namespace SeleniumFirst
+{
+    class PageObject
+    {
+        public PageObject()
+        {
+
+            PageFactory.InitElements(PropertiesCollection.driver, this);
+        }
+
+
+        [FindsBy(How = How.Id, Using = "et_pb_contact_name_1")]
+        public IWebElement TextBoxName { get; set; }
+
+
+        [FindsBy(How = How.Id, Using = "et_pb_contact_message_1")]
+        public IWebElement TextBoxMessage { get; set; }
+
+
+        [FindsBy(How = How.XPath, Using = "//div[@id='et_pb_contact_form_1']//button[@name='et_builder_submit_button'][contains(text(),'Submit')]")]
+        public IWebElement ClickSubmitButton { get; set; }
+
+
+        [FindsBy(How = How.CssSelector, Using = "input.input.et_pb_contact_captcha")]
+        public IWebElement BoxCaptchaNumber { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = "div.et-pb-contact-message > p")]
+        public IWebElement MessageSuccess { get; set; }
+
+        [FindsBy(How = How.ClassName, Using = "et_pb_contact_captcha_question")]
+        public IWebElement AfterAndBeforeCaptcha { get; set; }
+
+
+
+        public void SubmitForm(string textboxname, string textboxmessage, int boxtextnumber)
+
+        {
+            TextBoxName.EnterText(textboxname);
+            System.Threading.Thread.Sleep(3000);
+            TextBoxMessage.EnterText(textboxmessage);
+            System.Threading.Thread.Sleep(3000);
+            BoxCaptchaNumber.FillResultField(boxtextnumber);
+            System.Threading.Thread.Sleep(3000);
+            ClickSubmitButton.Click();
+            
+
+
+            
+
+
+
+        }
+        public String GetSucessErrorMsgText()
+        {
+            return MessageSuccess.Text;
+        }
+        public String MessageBox()
+        {
+            return TextBoxMessage.Text;
+        }
+        public String BoxNameField()
+        {
+            return AfterAndBeforeCaptcha.Text;
+        }
+    }
+}
---
+++ SeleniumFirst/Program.cs
@@ -0,0 +1,89 @@
+﻿using FillOutForm_tests;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SeleniumFirst
+{
+    class Program
+    {
+
+        static void Main1(string[] args)
+        {
+        }
+
+        [SetUp]
+        public void Initialize()
+        {
+            PropertiesCollection.driver = new ChromeDriver(@"C:\Users\vikto\Documents\Automation\libSources\WebDrivers\");
+            PropertiesCollection.driver.Navigate().GoToUrl("https://www.ultimateqa.com/filling-out-forms/"); // Go to URL 
+            Console.WriteLine("Opened URL"); //Print step Go to URL
+            PropertiesCollection.driver.Manage().Window.Maximize(); //Maximize Browser 
+            Console.WriteLine("Maximize the Browser");//Print step Maximize Browser
+
+
+        }
+        [TearDown]
+        public void CloseBrowser()
+        {
+            
+            PropertiesCollection.driver.Close(); //Close browser
+            Console.WriteLine("Closing Browser..."); //Print step Closing Browser
+            
+
+        }
+        [Test]
+        public void RunTest()
+        //-1 as sum
+
+        {
+            PageObject page = new PageObject();
+            //Catch Captcha Number Before pressing submit button
+            var beforesubmit = page.BoxNameField();
+            //Print Captcha Before submit
+            Console.WriteLine("Captcha before submit form: "+beforesubmit);
+
+            //Filling all fields on the right
+            page.SubmitForm("Viktor", "Hello there!", -1);
+            Console.WriteLine("Filling all fields on the right");
+
+            //Catch Captcha Number After pressing submit button
+            var aftersubmit = page.BoxNameField();
+            Console.WriteLine("Captcha after submit form: "+ aftersubmit);
+
+            //if captcha are diferent print False
+            Console.WriteLine("Are captcha numbers before and after pressing submit the same ? " + aftersubmit.Equals(beforesubmit));
+        }
+        [Test]
+        public void TestCorrectNumber()
+        //Test correct sum
+        {
+            //Catch Captcha number Before pressing submit button
+            PageObject page = new PageObject();
+            var beforesubmit = page.BoxNameField();
+            Console.WriteLine("Captcha before pressing submit button: " + beforesubmit);
+            
+            //Getting sum from the two numbers in captcha box
+            var correctResult = Calculation.Addition(beforesubmit);
+            
+            //Filling correct number into captcha box
+            page.SubmitForm("Viktor", "Hello there!", correctResult);
+            Console.WriteLine("Sum of the two numbers is " + correctResult);
+            
+            System.Threading.Thread.Sleep(6500);
+            //Verifying that success message is "Success"
+            var MessageSuccessSubmitForm = page.GetSucessErrorMsgText();
+            Console.WriteLine("Message is: " + MessageSuccessSubmitForm);
+            Assert.AreEqual("Success", MessageSuccessSubmitForm);
+
+        }
+    }
+}
